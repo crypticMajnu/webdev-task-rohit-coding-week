d2870497b0732912e7186f2676b142443246fb31
crypticMajnu <rohit.s.chalak@gmail.com>
2022-06-04 19:43:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,104 @@
+gsap.to('#downward_arrow', {
+    opacity: 0,
+    ease: 'none',
+    scrollTrigger: {
+        trigger: '.page1',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true
+    }
+});
+gsap.to('#face1', {
+    opacity: 1,
+    ease: 'none',
+    scrollTrigger: {
+        trigger: '.page1',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true
+    }
+});
+gsap.to('#face1', {
+    opacity: 0,
+    ease: 'none',
+    immediateRender: false,
+    scrollTrigger: {
+        trigger: '.page2',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true
+    }
+});
+gsap.to('#eyesclosed1', {
+    opacity: 1,
+    ease: 'none',
+    scrollTrigger: {
+        trigger: '.page2',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true
+    }
+});
+gsap.to('#eyesclosed1', {
+    opacity: 0,
+    ease: 'none',
+    immediateRender: false,
+    scrollTrigger: {
+        trigger: '.page3',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true
+    }
+});
+gsap.to('#stareyedface', {
+    opacity: 1,
+    ease: 'none',
+    scrollTrigger: {
+        trigger: '.page3',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true
+    }
+});
+gsap.to('#stareyedface', {
+    opacity: 0,
+    ease: 'none',
+    immediateRender: false,
+    scrollTrigger: {
+        trigger: '.page4',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true
+    }
+});
+gsap.to('#smilegamer', {
+    opacity: 1,
+    ease: 'none',
+    scrollTrigger: {
+        trigger: '.page4',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true
+    }
+});
+gsap.to('#smilegamer', {
+    opacity: 0,
+    ease: 'none',
+    immediateRender: false,
+    scrollTrigger: {
+        trigger: '.page5',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true
+    }
+});
+gsap.to('#face1', {
+    opacity: 1,
+    ease: 'none',
+    scrollTrigger: {
+        trigger: '.page5',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true
+    }
+});
